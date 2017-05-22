1309dea60f313d1f3e0d1e0a031152a6d0a17310
martafil <marta.dianna@gmail.com>
2017-05-22 16:57:28 +0300

    Tema 7 exercitiul 1

---
+++ tema6/app.py
@@ -0,0 +1,35 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, url_for
+import puncte_minim
+import radacini
+import json
+ 
+app = Flask(__name__, static_url_path = "", static_folder = "static")
+
+@app.route('/', methods = ['GET'])
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/info1', methods = ['POST'])
+def minimum_point1():
+    return jsonify({ "result": puncte_minim.get_minimum_point(puncte_minim.der_first, puncte_minim.der2_first)})
+
+@app.route('/info2', methods = ['POST'])
+def minimum_point2():
+    return jsonify({ "result": puncte_minim.get_minimum_point(puncte_minim.der_second, puncte_minim.der2_second)})
+
+@app.route('/info3', methods = ['POST'])
+def minimum_point3():
+    return jsonify({ "result": puncte_minim.get_minimum_point(puncte_minim.der_third, puncte_minim.der2_third)})
+
+
+@app.route('/info4', methods = ['POST'])
+def minimum_point4():
+    d = json.loads(request.data)["data"]
+    rad = radacini.find_roots([int(x) for x in d.split(" ")])
+    rad = [str(x) for x in rad]
+    return jsonify({ "result": rad })
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
